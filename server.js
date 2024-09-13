e1b4dbace8b863d0666536d57a0229bf26bec1c6
ICaoVy <icaovy2001@gmail.com>
2024-09-14 00:44:58 +0700

    update ejs

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const initApiRoutes = require('./routes');
 require("dotenv").config()
-// var createError = require('http-errors');
+var createError = require('http-errors');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var path = require('path');
+
 
 
 const hostName = process.env.HOST_NAME;
@@ -11,10 +13,15 @@ const port = process.env.PORT || 8000;
 
 const app = express()
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 initApiRoutes(app)
 
@@ -23,19 +30,19 @@ app.listen(port, hostName, () => {
 })
 
 
-// app.use((req, res, next) => {
-//     next(createError(404));
-// });
+app.use((req, res, next) => {
+    next(createError(404));
+});
 
-// app.use((err, req, res, next) => {
-//     // set locals, only providing error in development
-//     res.locals.message = err.message;
-//     res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use((err, req, res, next) => {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-//     // render the error page
-//     res.status(err.status || 500);
-//     res.render('error');
-// });
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
 
 
 module.exports = app;
