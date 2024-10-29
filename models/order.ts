c1bff60580656bcc25c220da10ae393328d05a9a
ICaoVy <icaovy2001@gmail.com>
2024-10-29 22:08:36 +0700

    update order schema

--- models/order.ts
+++ models/order.ts
@@ -4,7 +4,7 @@ const orderSchema = new Schema({
     order_date: { type: Date, required: true, default: Date.now },
     total_price: { type: Number, required: true, min: 0 },
     was_paid: { type: Boolean, default: false },
-    received_data: { type: Date, default: null },
+    received_date: { type: Date, default: null },
     order_description: { type: String, default: "" },
     cus_id: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "customers" },
     staff_id: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "staffs" },
