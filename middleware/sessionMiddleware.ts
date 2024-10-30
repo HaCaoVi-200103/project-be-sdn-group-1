595f470efbbc716fa7b465ba64b33abd2808b1e0
Gnahtop21 <lethang210903@gmial.com>
2024-10-30 22:34:56 +0700

    Add authentication-func

---
+++ middleware/sessionMiddleware.ts
@@ -0,0 +1,8 @@
+import { Request, Response, NextFunction } from 'express';
+
+export const isAuthenticated = (req: Request, res: Response, next: NextFunction) => {
+    if (req.session.id) {
+        return next();
+    }
+    return res.status(401).json({ message: 'Unauthorized' }); // User is not authenticated
+};
