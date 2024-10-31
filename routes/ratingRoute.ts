7f04ce4266745f6091545ceb7f3259b9899af7a6
ICaoVy <icaovy2001@gmail.com>
2024-10-31 19:31:18 +0700

    update route rating

--- routes/ratingRoute.ts
+++ routes/ratingRoute.ts
@@ -1,10 +1,13 @@
 import express, { Express } from "express";
-import { createRatingCake, getRatingByCakeId } from "../controllers/rating.controller";
+import { createRatingCake, deleteRatingById, getRatingByCakeId, getRatingById, updateRatingCake } from "../controllers/rating.controller";
 const route = express.Router();
 
 const ratingApiRoutes = (app: Express) => {
-    route.get('/:cakeId', getRatingByCakeId);
+    route.get('/:ratingId', getRatingById);
+    route.get('/list-rating/:cakeId', getRatingByCakeId);
     route.post('/', createRatingCake);
+    route.put('/:ratingId', updateRatingCake);
+    route.delete('/:ratingId', deleteRatingById);
 
     return app.use('/api/v1/rating', route);;
 }
