1065cd84a40ff730b0fed46cb1d120285d45e510
Quoc Hao <haotqce170322@fpt.edu.vn>
2024-11-01 07:46:52 +0700

    update create order

--- controllers/payManage.controller.ts
+++ controllers/payManage.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import Order from "../models/order";
-import mongoose from "mongoose";
+
 
 
 export const createOrderCashPayment = async (req: Request, res: Response) => {
