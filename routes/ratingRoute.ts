b6cf81c9425ca6ccf17fa5607df42a5288576923
thuan223 <thuantnn.it@gmail.com>
2024-11-04 04:25:43 +0700

    done

--- routes/ratingRoute.ts
+++ routes/ratingRoute.ts
@@ -1,21 +1,15 @@
 import express, { Express } from "express";
-import {
-  createRatingCake,
-  deleteRatingById,
-  getRatingByCakeId,
-  getRatingById,
-  updateRatingCake,
-} from "../controllers/rating.controller";
+import { createRatingCake, deleteRatingById, getRatingByCakeId, getRatingById, updateRatingCake } from "../controllers/rating.controller";
 const route = express.Router();
 
 const ratingApiRoutes = (app: Express) => {
-  route.get("/:ratingId", getRatingById);
-  route.get("/list-rating/:cakeId", getRatingByCakeId);
-  route.post("/", createRatingCake);
-  route.put("/:ratingId", updateRatingCake);
-  route.delete("/:ratingId", deleteRatingById);
+    route.get('/:ratingId', getRatingById);
+    route.get('/list-rating/:cakeId', getRatingByCakeId);
+    route.post('/', createRatingCake);
+    route.put('/:ratingId', updateRatingCake);
+    route.delete('/:ratingId', deleteRatingById);
 
-  return app.use("/api/v1/rating", route);
-};
+    return app.use('/api/v1/rating', route);;
+}
 
 export default ratingApiRoutes;
