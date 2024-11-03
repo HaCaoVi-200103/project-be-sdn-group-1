6fe052fc6eec9d2c64be5eb5398f46ac22e695d3
Nguyen Truong An <nguyentruongan15122003@gmail.com>
2024-11-04 01:16:39 +0700

    build: cakeController for statistic, router for cake statistic, topping management controller and router

--- routes/toppingManagementRoute.ts
+++ routes/toppingManagementRoute.ts
@@ -2,11 +2,10 @@ import express, { Express } from "express";
 import {
   addToppings,
   deleteTopping,
-  filterToppingsByPrice,
   getTopping,
   getSomeToppings,
-  searchToppings,
   updateTopping,
+  searchAndFilterToppings,
 } from "../controllers/toppingManagement.controller";
 import multer from "multer";
 
@@ -14,13 +13,12 @@ const route = express.Router();
 const upload = multer();
 
 const ToppingManagementRoute = (app: Express) => {
-  route.get("/", getSomeToppings);
+  route.post("/pagination", getSomeToppings);
   route.get("/:id", getTopping);
   route.post("/", upload.single("file"), addToppings);
   route.delete("/:id", deleteTopping);
   route.put("/:id", upload.single("file"), updateTopping);
-  route.post("/search", searchToppings);
-  route.post("/filter", filterToppingsByPrice);
+  route.post("/search", searchAndFilterToppings);
 
   return app.use("/api/v1/toppings", route);
 };
