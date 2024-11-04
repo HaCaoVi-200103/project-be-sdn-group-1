58505b911c4ea466a10cd842be4b2717acee13c8
thuan223 <thuantnn.it@gmail.com>
2024-11-04 15:21:09 +0700

    Add Login With Google

--- routes/authRouter.ts
+++ routes/authRouter.ts
@@ -1,26 +1,33 @@
 import express from "express";
-import { login, register } from "../controllers/auth.controller"; // Customer authentication
+import { login, register, loginByGoogle } from "../controllers/auth.controller"; // Customer authentication
 import { registerStaff, loginStaff } from "../controllers/auth.controller"; // Staff authentication
 import { isAuthenticated } from "../middleware/sessionMiddleware";
 
 const route = express.Router();
 
 // Customer Registration Route
-route.post('/register', register);
+route.post("/register", register);
 
 // Customer Login Route
-route.post('/login', login);
+route.post("/login", login);
 
 // Staff Registration Route
-route.post('/staff/register', registerStaff);
+route.post("/staff/register", registerStaff);
 
 // Staff Login Route
-route.post('/staff/login', loginStaff);
+route.post("/staff/login", loginStaff);
 
+//Login By Google
+route.post("/loginByGoogle", loginByGoogle);
 
 // Protected Route
-route.get('/protected', isAuthenticated, (req, res) => {
-    res.status(200).json({ message: 'You have access to this protected route', userId: req.session });
+route.get("/protected", isAuthenticated, (req, res) => {
+  res
+    .status(200)
+    .json({
+      message: "You have access to this protected route",
+      userId: req.session,
+    });
 });
 
 // Exporting the route to use in the app
